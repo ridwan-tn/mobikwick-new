becb8564a711b8f45e0b263d6e59a04b3c952aa6
Sonet Thomas <sonetthoma1997@gmail.com>
2021-06-21 09:12:25 +0530

    add mutual fund all pages and add a library matreial ui lab for autocomplete component

--- src/pages/mutualfunds/mutual.js
+++ src/pages/mutualfunds/mutual.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import React from "react";
 
-export default function mutual() {
-    return (
-        <div style={{textAlign:"center"}}>
-           <h1>Mutual Funds</h1> 
-           <h2>Sonet updated the one  for git testing</h2>
-        </div>
-    )
+import Mutual from './MnotRes'
+import Mresponsive from './Mresponsive'
+const viewportContext = React.createContext({});
+
+const ViewportProvider = ({ children }) => {
+  const [width, setWidth] = React.useState(window.innerWidth);
+  const [height, setHeight] = React.useState(window.innerHeight);
+  const handleWindowResize = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("resize", handleWindowResize);
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, []);
+
+  return (
+    <viewportContext.Provider value={{ width, height }}>
+      {children}
+    </viewportContext.Provider>
+  );
+};
+
+const useViewport = () => {
+  const { width, height } = React.useContext(viewportContext);
+  return { width, height };
+};
+
+const MobileComponent = () => <p><Mresponsive/></p>;
+const DesktopComponent = () => <p><Mutual/></p>;
+
+const MyComponent = () => {
+  const { width } = useViewport();
+  const breakpoint = 875;
+
+  return width < breakpoint ? <MobileComponent /> : <DesktopComponent />;
+};
+
+export default function MutualFRes() {
+  return (
+    <ViewportProvider>
+      <MyComponent />
+    </ViewportProvider>
+  );
 }
+
